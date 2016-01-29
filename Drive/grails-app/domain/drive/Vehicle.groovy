6ef5fa474ef0188681061ceb3131f88e5f653da2
Tom Jackman <tom.dls@hotmail.com>
2016-01-29 16:47:36 +0000

    Added new fields.

--- Drive/grails-app/domain/drive/Vehicle.groovy
+++ Drive/grails-app/domain/drive/Vehicle.groovy
@@ -4,20 +4,42 @@ class Vehicle {
 	
 	String identifier
 	Driver driver
-	static hasMany = [journeys : Journey]
-	String manufacturer
-	String model
-	int year
-	double engineSize
-	String fuelType
+	static hasMany = [journeys : Journey, features : String, colourNames : String, colourCodes : String]
+	
+	String make, model, engineConfiguration, fuelType, manufacturerEngineCode, drivenWheels, transmissionType, vehicleStyle, vehicleClass, numberOfSpeeds
+	Integer year, cylinders, engineDisplacement, totalEngineValves, numOfDoors, mpgHighway, mpgCity, newPrice, usedPrice
+	Double compressionRatio, engineSize, horsepower, torque
+	
 
     static constraints = {
 		identifier blank:false
-		driver()
-		manufacturer()
-		model()
-		year()
-		engineSize()
-		fuelType()
+		driver blank:false
+		make blank:false
+		model blank:false
+		year blank:false
+		engineConfiguration nullable:true
+		engineDisplacement nullable:true
+		manufacturerEngineCode nullable:true
+		totalEngineValves nullable:true
+		cylinders nullable:true
+		compressionRatio nullable:true
+		engineSize nullable:true
+		horsepower nullable:true
+		torque nullable:true
+		fuelType nullable:true
+		drivenWheels nullable:true
+		transmissionType nullable:true
+		vehicleStyle nullable:true
+		vehicleClass nullable: true
+		numberOfSpeeds nullable:true
+		numOfDoors nullable:true
+		mpgHighway nullable:true
+		mpgCity nullable:true
+		newPrice nullable:true
+		usedPrice nullable:true
+		colourNames nullable:true
+		colourCodes nullable:true 
+		features nullable:true
     }
+	
 }

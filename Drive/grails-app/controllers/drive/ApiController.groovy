d8a1793616481cc12e3fb8884a5be444726516fd
Tom Jackman <tom.dls@hotmail.com>
2016-01-29 18:36:22 +0000

    Added code comments.

--- Drive/grails-app/controllers/drive/ApiController.groovy
+++ Drive/grails-app/controllers/drive/ApiController.groovy
@@ -4,47 +4,77 @@ import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 import groovy.json.JsonSlurper
 
+/**
+ * The API Controller is responsible for the handling of the interactions between the mobile app and the web app.
+ */
+
 @Transactional(readOnly = false)
 class ApiController {
 	
+	// import vehicle and journey data manipulation services
 	def vehicleService
+	def journeyDataManipulationService
 
+	// Data Response Formats
     static responseFormats = ['json', 'xml']
+	
+	// Allowed Methods to Interact with Resources
     static allowedMethods = [addNewJourney: "POST", addNewVehicle: "POST",save: "POST", update: "PUT", delete: "DELETE"]
 
+	/**
+	 * 
+	 * @param max - the maximum number of items to return
+ 	*/
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond Api.list(params), [status: OK]
     }
-	
-	// NEW ### curl -i -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/json" -d '{"age": 59, "dateOfBirth": 1994-11-11, "country":italy}' localhost:8080/Drive/api/addNewVehicle
-	
-	
+		
+	/**
+	 * This method adds a new driver and a vehicle to the database based on the data sent from the phone.
+	 * @param properties - The Driver/Vehicle data sent from the phone in JSON format
+ 	*/
 	@Transactional
 	def addNewVehicle(String properties) 
 	{
-		def data = request.JSON										// Data contains vehicle + driver information 
+		// Collects the JSON data sent from the phone 
+		def data = request.JSON										
 		
+		// Picking out Driver information and car data
 		def dateOfBirth = data.dateOfBirth;
 		def gender = data.gender;
 		def country = data.country;
-		
-		def jsonSlurper = new JsonSlurper()
 		def carData = data.carData
-						
+		
+		// Generate a new ID for the vehicle. This ID will be globally unique.
 		String id = vehicleService.generateUniqueID();
 		
-		if (dateOfBirth == null | gender == null | country == null | carData.year.year == null) {
-			render status: NOT_ACCEPTABLE							// Send 405, some of the data is null
+		// Check if some of the key attributes are null
+		if (dateOfBirth == null | gender == null | country == null | carData.model.name== null | carData.make.name == null | carData.year.year == null) {
+			// Send 405 if any of the key data is null
+			render status: NOT_ACCEPTABLE							
 			return
 		}
-				
+		
+		// Create a new driver and save them to the database	
 		def driver = new Driver("gender": gender, "dateOfBirth": Date.parse( 'dd-MM-yyyy', dateOfBirth ), "country": country)
+		
+		// validate that the driver attributes obey the constraints set out in the domain class
+		driver.validate()
+		if (driver.hasErrors()) {
+			render status: NOT_ACCEPTABLE
+			println driver.errors
+			return
+		}
+
+		// save the driver to the database
 		driver.save()
 		
+		// Sets to hold colour names/codes
 		def colourNames = []
 		def colourCodes = []
 		
+		// Extract colour names/codes from the carData structure and ass them to the sets
 		for(int i = 0; i < carData.colors.size(); i++)
 		{
 			for(int j = 0; j < carData.colors[i].options.size(); j++)
@@ -55,8 +85,10 @@ class ApiController {
 
 		}
 		
+		// Set to hold cars notable features list
 		def features = []
 		
+		// Extract features from the carData structure and ass them to the features set
 		for(int i = 0; i < carData.options.size(); i++)
 		{
 			for(int j = 0; j < carData.options[i].options.size(); j++)
@@ -65,7 +97,7 @@ class ApiController {
 			}
 		}
 		
-			
+		// create the new vehicle object
 		def vehicle = new Vehicle("identifier": id, 
 					"driver": driver, 
 					"year": carData.year?.year,
@@ -95,6 +127,7 @@ class ApiController {
 					"colourCodes": colourCodes,
 					"features": features)
 		
+		// validate that the vehicle attributes obey the constraints set out in the domain class
 		vehicle.validate()
 		if (vehicle.hasErrors()) {
 			render status: NOT_ACCEPTABLE
@@ -102,21 +135,30 @@ class ApiController {
 			return
 		}
 		
-		vehicle.save()		
-			
-		render status: CREATED											// Send 200 OK, all data is 
+		// save the vehicle to the database
+		vehicle.save()	
+
+		// New driver/vehicle created sucessfully
+		render status: CREATED											
 	}
 	
+	/**
+	 * 
+ 	*/
 	@Transactional
 	def addNewJourney(String vehicleData)
 	{
 		def data = request.JSON	
 		
 		println data
-				
-		render status: CREATED										// Send 200 OK, all data is
+		
+		// Send 200 OK, all data is valid and saved to DB
+		render status: CREATED
 	}
 	
+	/**
+	 * 
+ 	*/
     @Transactional
     def save(Api apiInstance) {
         if (apiInstance == null) {
@@ -134,6 +176,9 @@ class ApiController {
         respond apiInstance, [status: CREATED]
     }
 
+    /**
+	 * 
+ 	*/
     @Transactional
     def update(Api apiInstance) {
         if (apiInstance == null) {
@@ -151,6 +196,9 @@ class ApiController {
         respond apiInstance, [status: OK]
     }
 
+    /**
+	 * 
+ 	*/
     @Transactional
     def delete(Api apiInstance) {
 

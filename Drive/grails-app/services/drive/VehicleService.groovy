655b12d2cf01cba952dfc7df7391e5d6b6850168
Tom Jackman <tom.dls@hotmail.com>
2016-01-29 16:48:11 +0000

    Added generation of unique ID.

--- Drive/grails-app/services/drive/VehicleService.groovy
+++ Drive/grails-app/services/drive/VehicleService.groovy
@@ -5,18 +5,17 @@ import grails.transaction.Transactional
 @Transactional
 class VehicleService {
 	
-	def addNewDriver(gender, dateOfBirth, country)
-	{
-		new Driver("gender":gender, "dateOfBirth": Date.parse( 'dd-MM-yyyy', dateOfBirth ), "country":country).save()
-	}
-
-    def addNewVehicle() {
-
-    }
-	
 	def generateUniqueID()
 	{
+		char[] chars = "abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+		StringBuilder stringBuilder = new StringBuilder();
 		
+		Random random = new Random();
+		
+		for (int i = 0; i < 10; i++) {
+			char character = chars[random.nextInt(chars.length)];
+			stringBuilder.append(character);
+		}
+		return stringBuilder.toString();
 	}
-	
 }

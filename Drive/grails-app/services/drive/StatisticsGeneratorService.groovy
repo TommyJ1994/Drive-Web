cf0f66a0e87b3aab50e2018635ebe9661da374b4
Tom Jackman <tom.dls@hotmail.com>
2016-02-26 15:06:09 +0000

    Added stats generators.

--- Drive/grails-app/services/drive/StatisticsGeneratorService.groovy
+++ Drive/grails-app/services/drive/StatisticsGeneratorService.groovy
@@ -12,37 +12,109 @@ class StatisticsGeneratorService {
 
 	/**
 	 * Get the average speed of the vehicle
+	 * @param speedData - the speed values recorded on a journey
 	 */
-    def getAverageSpeed() {
-
+    def getAverageSpeed(def speedData) 
+	{
+		int sum = 0;
+		
+		for(int i = 0; i < speedData.size(); i++)
+		{
+			sum += speedData[i]
+		}
+		
+		return sum / speedData.size()
     }
 	
 	/**
 	 * Get the average rpm of the vehicle
+	 * @rpmData - the rpm values recorded on a journey
 	 */
-	def getAverageRPM() {
-	
+	def getAverageRPM(def rpmData) 
+	{
+		int sum = 0;
+		
+		for(int i = 0; i < rpmData.size(); i++)
+		{
+			sum += rpmData[i]
+		}
+		
+		return sum / rpmData.size()
 	}
 	
 	/**
-	 * Get the top speed of the vehice
+	 * Get the top speed of the vehicle
+	 * @param speedData - the speed values recorded on a journey
 	 */
-	def getTopSpeed() {
-	
+	def getTopSpeed(def speedData) 
+	{
+		int topSpeed = speedData[0];
+		
+		for(int i = 0; i < speedData.size(); i++)
+		{
+			if(speedData[i] > topSpeed)
+			{
+				topSpeed = speedData[i]
+			}
+		}
+		return topSpeed
 	}
 	
 	/**
-	 * Get heavy braking counts
+	 * Get the top engine RPM
+	 * @rpmData - the rpm values recorded on a journey
 	 */
-	def getHeavyBrakingCount() {
-	
+	def getTopRPM(def rpmData) 
+	{
+		int topSpeed = rpmData[0];
+		
+		for(int i = 0; i < rpmData.size(); i++)
+		{
+			if(rpmData[i] > topSpeed)
+			{
+				topSpeed = rpmData[i]
+			}
+		}
+		return topSpeed
 	}
 	
 	/**
-	 * Get heavy acceleration counts
+	 * Get the number of times the driver braked heavily during a journey
+	 * @param speedData - the speed values recorded on a journey
 	 */
-	def getHeavyAccelerationCount() {
+	def getHeavyBrakingCount(def speedData) 
+	{
+		int count = 0
+		
+		for(int i = 0; i < speedData.size(); i++)
+		{
+			// check that the end of the list has not been reached
+			if(speedData[i + 5] != null && speedData[i] - speedData[i + 1] > 7)
+			{
+				count++;
+				i+= 10 // Prevent a long brake being counted as multiple heavy brakings
+			}
+		}
+		return count
+	}
 	
+	/**
+	 * Get the number of times the driver accelerated heavily during a journey
+	 */
+	def getHeavyAccelerationCount(def speedData) 
+	{
+		int count = 0
+		
+		for(int i = 0; i < speedData.size(); i++)
+		{
+			// check that the end of the list has not been reached
+			if(speedData[i + 5] != null && speedData[i + 1] - speedData[i] > 7)
+			{
+				count++;
+				i+= 10 // Prevent a long acceleration being counted as multiple heavy accelerations
+			}
+		}
+		return count
 	}
 	
 }

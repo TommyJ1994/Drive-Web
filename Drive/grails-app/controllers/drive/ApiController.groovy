a9ffecfc4c2c368c9aa56d4913c89ac51da5430e
Tom Jackman <tom.dls@hotmail.com>
2016-02-26 14:43:11 +0000

    Added storage of journeys.

--- Drive/grails-app/controllers/drive/ApiController.groovy
+++ Drive/grails-app/controllers/drive/ApiController.groovy
@@ -3,6 +3,7 @@ package drive
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 import groovy.json.JsonSlurper
+import java.text.SimpleDateFormat
 
 /**
  * The API Controller is responsible for the handling of the interactions between the mobile app and the web app.
@@ -127,15 +128,157 @@ class ApiController {
 	 * after the data manipulation has been carried out
  	*/
 	@Transactional
-	def addNewJourney(String vehicleData)
+	def addNewJourney(String dataFromPhone)
 	{
-		def data = request.JSON	
+		def data = request.JSON
 		
-		// Send the data to be prcoessed by the manuipulation service
-		journeyDataManipulationService.processData(data);
+		def vehicle = Vehicle.findByIdentifier(data.vehicleID);
 		
-		// Send 200 OK, all data is valid and saved to DB
-		render status: CREATED
+		def startTime = new Date().copyWith(
+		    year: data.startTime.year, 
+		    month: data.startTime.month, 
+		    dayOfMonth: data.startTime.dayOfMonth, 
+		    hourOfDay: data.startTime.hourOfDay,
+		    minute: data.startTime.minute,
+		    second: data.startTime.second)
+		
+		def endTime = new Date().copyWith(
+			year: data.endTime.year,
+			month: data.endTime.month,
+			dayOfMonth: data.endTime.dayOfMonth,
+			hourOfDay: data.endTime.hourOfDay,
+			minute: data.endTime.minute,
+			second: data.endTime.second)
+		
+		def journeyTimeLength = 0;
+		
+		def journeyData = data.journeyData;
+		
+		// Send the data to be processed by the manipulation service
+		def result = journeyDataManipulationService.process(journeyData);
+				
+		// List of human readable sensor names
+		def sensorNames = ["Calculated Engine Load",
+			"Engine Coolant Temperature",
+			"Fuel Pressure",
+			"Intake Manifold Absolute Pressure",
+			"Engine RPM",
+			"Vehicle Speed",
+			"Intake Air Temperature",
+			"MAF Air Flow Rate",
+			"Throttle Position",
+			"Oxygen Sensor Voltage 1",
+			"Oxygen Sensor Voltage 2",
+			"Barometric Pressure",
+			"Catalyst Temperature Bank 1 Sensor 1",
+			"Catalyst Temperature Bank 2 Sensor 1",
+			"Catalyst Temperature Bank 1 Sensor 2",
+			"Catalyst Temperature Bank 2 Sensor 2",
+			"Absolute Load Value",
+			"Relative Throttle Position",
+			"Ambient Air Temperature",
+			"Commanded Throttle Actuator",
+			"Relative Accelerator Pedal Position",
+			"Engine Oil Temperature",
+			"Fuel Injection Timing",
+			"Engine Fuel Rate",
+			"Drivers Demand Engine Torque",
+			"Actual Engine Torque",
+			"Engine Reference Torque"
+			];
+		
+		// List of human readable sensor descriptions
+		def sensorDescriptions = ["Indicates a percentage of peak available torque. Reaches 100% at wide open throttle at any altitude or RPM for both naturally aspirated and boosted engines.",
+			"Engine coolant temperature derived from an engine coolant temperature sensor or a cylinder head temperature sensor.",
+			"Indicates the fuel rail pressure at the engine referenced to atmosphere (gauge pressure).",
+			"Indicates the manifold pressure derived from a Manifold Absolute Pressure sensor.",
+			"The current engine revolutions per minute value.",
+			"The vehicle road speed.",
+			"The intake manifold air temperature.",
+			"Airflow rate as measured by a vehicle MAF sensor or an equivalent source.",
+			"The absolute throttle position (not the relative or learned) throttle position. Usually above 0% at idle and less than 100% at full throttle.",
+			"Measures the proportion of oxygen (O2) in the gas or liquid being analysed for the first Oxygen sensor.",
+			"Measures the proportion of oxygen (O2) in the gas or liquid being analysed for the second Oxygen sensor.",
+			"Barometric pressure normally obtained from a dedicated barometric sensor.",
+			"The catalyst substrate temperature in bank 1, sensor 1.",
+			"The catalyst substrate temperature in bank 2, sensor 1.",
+			"The catalyst substrate temperature in bank 1, sensor 2.",
+			"The catalyst substrate temperature in bank 2, sensor 2.",
+			"Indicates a percentage of peak available torque.",
+			"The relative or learned throttle position.",
+			"The ambient air temperature.",
+			"The commanded throttle actuator.  0% fully closed and 100% fully open.",
+			"Relative or learned pedal position.",
+			"The engine oil temperature.",
+			"The start of the mail fuel injection relative to Top Dead Center (TDC). Positive degrees indicate before TDC. Negative degrees indicate after TDC.",
+			"The amount of fuel consumed by the engine per unit of time in liters per hour.",
+			"The requested torque output of the engine by the driver.",
+			"The calculated output torque of the engine.",
+			"The engine reference torque value. This value does not change."
+			];
+		
+		int count = 0;
+		
+		def sensorList = []
+		
+		def newJourney = new Journey("sensors": sensorList,
+			"startTime": startTime,
+			"endTime": endTime,
+			"journeyTimeLength": journeyTimeLength,
+			"topSpeed": result.statistics[0],
+			"topRPM": result.statistics[1],
+			"averageSpeed": result.statistics[2],
+			"averageRPM": result.statistics[3],
+			"heavyAccelerationCount": result.statistics[4],
+			"heavyBrakingCount": result.statistics[5],
+			"vehicle": vehicle
+			);
+		
+		newJourney.validate();
+		if (newJourney.hasErrors()) {
+			respond status: NOT_ACCEPTABLE
+			println newJourney.errors
+			return
+		}
+					
+				
+			result.sensors.each
+			{
+				sensorData ->
+								
+				String sensorName = sensorNames[count];
+				String sensorDescription = sensorDescriptions[count];
+				def points = [];
+				
+				def newSensor = new Sensor("name": sensorName, "description": sensorDescription, "points": [], "journey": newJourney);
+				
+				
+				for(int i = 0; i < sensorData.size(); i++)
+					{
+						def point = new Point("value": sensorData[i], "sensor": newSensor)
+						points << point					
+					}
+					
+					newSensor.points += points;
+				
+				newSensor.validate();
+				if (newSensor.hasErrors()) {
+					respond status: NOT_ACCEPTABLE
+					println newSensor.errors
+					return
+				}
+				
+				sensorList.add(newSensor)
+				count++
+			}
+			
+			
+			newJourney.sensors = sensorList;
+			newJourney.save();
+			
+								
+		// Send 200 OK, all data is valid and saved to DB successfully.
+		respond status: CREATED
 	}
 
     /**

2f8273612e709b7838a4820b5f1c1e7588b7de50
Tom Jackman <tom.dls@hotmail.com>
2016-03-02 09:35:15 +0000

    Added MongoDB.

--- Drive/grails-app/conf/Config.groovy
+++ Drive/grails-app/conf/Config.groovy
@@ -85,6 +85,13 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+// mongodb
+grails {
+	mongodb {
+		databaseName = 'Drive'
+	}
+}
+
 environments {
     development {
         grails.logging.jul.usebridge = true

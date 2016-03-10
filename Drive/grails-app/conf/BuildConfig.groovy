3c5911dc91ae6185d9e222ca620460f7e23677a4
Tom Jackman <tom.dls@hotmail.com>
2016-03-10 14:57:09 +0000

    Added google charts and qr code.

--- Drive/grails-app/conf/BuildConfig.groovy
+++ Drive/grails-app/conf/BuildConfig.groovy
@@ -62,6 +62,8 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
 		compile ":mongodb:3.0.3"
+		compile "org.grails.plugins:google-visualization:1.0.1"
+		compile(':qrcode:0.7')
 
         // plugins needed at runtime but not for compilation
         //runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
